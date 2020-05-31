77c95ef4eec19b395f28d3b87b94a3366f43ebe3
Prabin Shrestha <mail.prabin.shrestha@gmail.com>
2020-05-31 13:07:49 +0545

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { TextField, Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: '',
+      mobileNo: '',
+      amount: ''
+    }
+  }
+
+
+  componentDidMount() {
+    window.addEventListener('message', (data) => {
+      this.setState({ data: data })
+    })
+  }
+
+  handleValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  submitData = () => {
+
+    this.setState({
+      customerId: this.customerId.value
+    })
+
+    window.postMessage(this.state.amount, '*')
+  }
+
+  render() {
+    return (
+      <div className='App' ref={ref => this.rv = ref}>
+        <form className='form'>
+          <input ref={ref => this.customerId = ref} id='customerId' type='hidden' />
+
+          <TextField style={styles.input}
+            label='Mobile Number'
+            name='mobileNo'
+            value={this.state.mobileNo}
+            onChange={this.handleValueChange}
+          />
+
+          <TextField style={styles.input}
+            label='Amount'
+            name='amount'
+            value={this.state.amount}
+            onChange={this.handleValueChange}
+          />
+
+          <Button style={styles.button}
+            variant='contained'
+            onClick={this.submitData}>
+            Top Up Now
+          </Button>
+
+        </form>
+        {this.state.customerId}
+        <br />
+        {this.state.data.data}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+const styles = {
+  input: {
+    marginTop: 16,
+  },
+  button: {
+    marginTop: 24,
+    backgroundColor: '#b71c23',
+    color: '#fff'
+  }
+}
